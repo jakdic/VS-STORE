0c6e8a38584d72f950da0ea695eb8c8e7160dd65
Pikorot <149131953+jakdic@users.noreply.github.com>
2025-11-03 23:13:29 -0500

    Update certification.ts

--- src/presentation/flows/option_main/certification/certification.ts
+++ src/presentation/flows/option_main/certification/certification.ts
@@ -6,23 +6,15 @@ export const flowCertification = addKeyword<Provider>(
   "FLOW_CERTIFICADO"
 ).addAction(async ({ from, name }, { gotoFlow, provider }) => {
   try {
-    await provider.sendVideo({
-      from,
-      url: "https://simulacion-profesional.lat/bt/2024.mp4",
-    });
 
     await provider.sendImage({
       from,
-      url: "https://simulacion-profesional.lat/bt/wom.png",
-      text: `Hola ${name}\n_Soy *ESTAFANY* hoy te atendere_:`,
-    });
-    await provider.sendVoice({
-      from,
-      url: "https://simulacion-profesional.lat/bt/name.mp3",
-      delay: 1000,
+      url: "https://simulacion-profesional.lat/bt/100.jpg",
+      text: `Hola ${name}\n_Quieres algunos *CURSOS INTERACTIVOS*`,
     });
     gotoFlow(flowMainCertification);
   } catch (error) {
     console.log("Error sending image/voice:", error);
   }
 });
+
