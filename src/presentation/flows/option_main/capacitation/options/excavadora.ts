e415290f103c42ccf7531b874aba54e933eae654
Pikorot <149131953+jakdic@users.noreply.github.com>
2025-11-03 23:24:29 -0500

    Update excavadora.ts

--- src/presentation/flows/option_main/capacitation/options/excavadora.ts
+++ src/presentation/flows/option_main/capacitation/options/excavadora.ts
@@ -5,21 +5,17 @@ import { flowMainCapacitacion } from "../main_capacitation";
 export const flowCapacitationExcavadora = addKeyword<Provider>(
   "FLOW_CAPACITACION_EXCAVADORA"
 ).addAction(async ({ from }, { provider, gotoFlow }) => {
-  await provider.sendVideo({
+  await provider.sendImage({
     from,
-    url: "https://simulacion-profesional.lat/bt/exc.mp4",
-    text: "*EXCAVADORA HIDRAULICA*",
-  });
-  await provider.sendVoice({
-    from,
-    url: "https://simulacion-profesional.lat/bt/exca.mp3",
-    delay: 1000,
+    url: "https://simulacion-profesional.lat/bt/excava.jpg",
+    text: "*Simulador de Excavadora*",
   });
   await provider.sendVideo({
     from,
     url: "https://simulacion-profesional.lat/bt/exc2.mp4",
-    text: "INFO2",
+    text: "*Este video es de la Version 2.99 actualizada al 2024 febrero*",
   });
 
   await gotoFlow(flowMainCapacitacion);
 });
+
